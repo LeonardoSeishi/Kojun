681fb4e7866282ac5d290bddc5b9683d57c72b42
LeonardoSeishi <leonardosyamazaki@gmail.com>
2022-07-15 23:44:08 -0300

    versao atualizada

--- PROLOG/main.pl
+++ PROLOG/main.pl
@@ -4,26 +4,28 @@
 tam_R(1,2).
 tam_R(2,5).*/
 
-tam_R(0 ,4).
-tam_R(1 ,4).
-tam_R(2 ,5).
-tam_R(3 ,1).
-tam_R(4 ,1).
-tam_R(5 ,2).
-tam_R(6 ,4).
-tam_R(7 ,2).
-tam_R(8 ,6).
-tam_R(9 ,2).
-tam_R(10,5).
-
 kojun(L) :-
+    length(L, 6), maplist(same_length(L), L),
     append(L, S),
     maplist(intervalo, S),
     maplist(limite_regiao, S),
     maplist(adjacente, L),
     transpose(L, Columns),
     maplist(adjacente,Columns),
+    findall(X,igual([0,X],S),R0),
+    findall(X,igual([1,X],S),R1),
+    findall(X,igual([2,X],S),R2),
+    findall(X,igual([3,X],S),R3),
+    findall(X,igual([4,X],S),R4),
+    findall(X,igual([5,X],S),R5),
+    findall(X,igual([6,X],S),R6),
+    findall(X,igual([7,X],S),R7),
+    findall(X,igual([8,X],S),R8),
+    findall(X,igual([9,X],S),R9),
+    findall(X,igual([10,X],S),R10),
+    Rs = [R0,R1,R2,R3,R4,R5,R6,R7,R8,R9,R10], diferente(Rs),
     maplist(acima_maior, Columns).
+
 
 problem(1, [[[0,2],[0,1],[1,2]],
             [[2,3],[2,4],[1,1]],
@@ -37,23 +39,39 @@ problem(2, [[[0,_],[1,_],[1,4],[1 ,_],[2 ,2],[3 ,_]],
             [[7,_],[8,_],[8,_],[10,_],[10,3],[6 ,_]],
             [[8,6],[8,2],[8,_],[10,2],[10,_],[10,5]]]).
 
-/*adjacente([[R1,X1],[R2,X2],[R3,X3]]) :-
-    (all_distinct([X1,X2]); not(all_distinct([R1,R2]))),
-    (all_distinct([X3,X2]); not(all_distinct([R3,R2]))).
-    %append([[R2,X2]],T,L), adjacente(L).*/
 
-adjacente([[_,_]]).
-adjacente([[_,X1],[R2,X2]|T]) :-
-    all_distinct([X1,X2]),
-    append([[R2,X2]],T,L), adjacente(L).
+tam_R(0 ,4).
+tam_R(1 ,4).
+tam_R(2 ,5).
+tam_R(3 ,1).
+tam_R(4 ,1).
+tam_R(5 ,2).
+tam_R(6 ,4).
+tam_R(7 ,2).
+tam_R(8 ,6).
+tam_R(9 ,2).
+tam_R(10,5).
+
+diferente([H]) :-
+    all_distinct(H).
+diferente([H|T]) :-
+    all_distinct(H),
+    diferente(T).
 
 acima_maior([[_,_]]).
 acima_maior([[R1,X1],[R2,X2]|T]) :-
-    ((R1 #\= R2); (X1 > X2)),
-    append([[R2,X2]],T,L), 
+    ((R1 #\= R2);
+    (X1 #> X2)),
+    append([[R2,X2]],T,L),
     acima_maior(L).
 
+igual([R,X],[[R1,X1]|_]) :- R #= R1, X #= X1.
+igual([R,X],[_|T]) :- igual([R,X],T).
 
-intervalo([_,X]) :- X in 1..5.
+adjacente([[_,_]]).
+adjacente([[_,X1],[R2,X2]|T]) :-
+    all_distinct([X1,X2]),append([[R2,X2]],T,L), adjacente(L).
+
+intervalo([_,X]) :- X in 1..6.
 
 limite_regiao([R,X]) :-tam_R(R,T), X #=< T.
